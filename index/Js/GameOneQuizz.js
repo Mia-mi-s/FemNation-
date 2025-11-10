fc93495b399e1b313afa27a07fc90d0f15c2f5ed
Mia <soumia6431@gmail.com>
2025-11-10 11:52:21 +0100

    made the conditions for the quizz game need to fix the counter and add more questions ect

--- index/Js/GameOneQuizz.js
+++ index/Js/GameOneQuizz.js
@@ -7,9 +7,34 @@ const MySubmit = document.getElementById("MySubmit");
 const AnswerResult = document.getElementById("AnswerResult");
 const points = document.getElementById("points");
 
+// need to make a var that counts the points et ensuite l'afficher
+
+//learn how to handle lists so you can makea for loop
+
+let PointCounter = 0;
+let IsCorrect = true;
+
+if(IsCorrect){
+    PointCounter = PointCounter + 5;
+}
+
+
 MySubmit.onclick = function(){
     if(MyCheckBox1956.checked){
-        AnswerResult.textContent = "Nope try another"; 
+        AnswerResult.textContent = "Nope try another";
+        points.textContent = PointCounter
     }
-    if
+    if(MyCheckBox1850.checked){
+        AnswerResult.textContent = "this answer is correct";
+        points.textContent = "5 points gained"
+        points.textContent = "0 points gained"
+    }
+    if(MyCheckBox2025.checked){
+        AnswerResult.textContent = "Nope try another";
+        points.textContent = "0 points gained"
+    }
+    if(MyCheckBox1790.checked){
+        AnswerResult.textContent = "Nope try another";
+        points.textContent = "0 points gained"
+    }
 }
