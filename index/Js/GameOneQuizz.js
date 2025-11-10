3de888848556c2203cbe6ba3f7c9882db26f1aa9
Mia <soumia6431@gmail.com>
2025-11-10 10:27:07 +0100

    Made a page for game one linked it to its js file and started on the quiz game, watched checked box property vid from bro code added margin bc of the nav bar

---
+++ index/Js/GameOneQuizz.js
@@ -0,0 +1,15 @@
+const MyCheckBox1956 = document.getElementById("MyCheckBox1956");
+const MyCheckBox1850 = document.getElementById("MyCheckBox1850");
+const MyCheckBox2025 = document.getElementById("MyCheckBox2025");
+const MyCheckBox1790 = document.getElementById("MyCheckBox1790");
+
+const MySubmit = document.getElementById("MySubmit");
+const AnswerResult = document.getElementById("AnswerResult");
+const points = document.getElementById("points");
+
+MySubmit.onclick = function(){
+    if(MyCheckBox1956.checked){
+        AnswerResult.textContent = "Nope try another"; 
+    }
+    if
+}
