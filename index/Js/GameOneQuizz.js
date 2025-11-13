0c51c7ff3331a7843641101d7cadd53fc274125b
Mia <soumia6431@gmail.com>
2025-11-13 22:58:50 +0100

    need to fix the counter in js added 1 more question in total should be 10

--- index/Js/GameOneQuizz.js
+++ index/Js/GameOneQuizz.js
@@ -3,6 +3,13 @@ const MyCheckBox1850 = document.getElementById("MyCheckBox1850");
 const MyCheckBox2025 = document.getElementById("MyCheckBox2025");
 const MyCheckBox1790 = document.getElementById("MyCheckBox1790");
 
+
+const MyCheckExecuted = document.getElementById("MyCheckExecuted");
+const MyCheckBoxkilled = document.getElementById("MyCheckBoxkilled");
+const MyCheckBoxMurdered = document.getElementById("MyCheckBoxMurdered");
+const MyCheckBoxAssassinated = document.getElementById("MyCheckBoxAssassinated");
+
+
 const MySubmit = document.getElementById("MySubmit");
 const AnswerResult = document.getElementById("AnswerResult");
 const points = document.getElementById("points");
@@ -23,11 +30,11 @@ MySubmit.onclick = function(){
     if(MyCheckBox1956.checked){
         AnswerResult.textContent = "Nope try another";
         points.textContent = PointCounter
+        points.textContent = "0 points gained"
     }
     if(MyCheckBox1850.checked){
         AnswerResult.textContent = "this answer is correct";
         points.textContent = "5 points gained"
-        points.textContent = "0 points gained"
     }
     if(MyCheckBox2025.checked){
         AnswerResult.textContent = "Nope try another";
@@ -37,4 +44,25 @@ MySubmit.onclick = function(){
         AnswerResult.textContent = "Nope try another";
         points.textContent = "0 points gained"
     }
-}
+
+
+
+MySubmit.onclick = function(){
+    if(MyCheckExecuted.checked){
+        AnswerResult.textContent = "this answer is correct";
+        points.textContent = PointCounter
+        points.textContent = "0 points gained"
+    }
+    if(MyCheckBoxkilled.checked){
+        AnswerResult.textContent = "Nope try another";
+        points.textContent = "5 points gained"
+    }
+    if(MyCheckBoxMurdered.checked){
+        AnswerResult.textContent = "Nope try another";
+        points.textContent = "0 points gained"
+    }
+    if(MyCheckBoxAssassinated.checked){
+        AnswerResult.textContent = "Nope try another";
+        points.textContent = "0 points gained"
+    }
+}}
